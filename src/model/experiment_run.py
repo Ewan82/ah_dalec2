2abd8c2e6073610de45b7797bd0d0e17e34393ae
Ewan Pinnington <ewan.pinnington@gmail.com>
2016-11-15 11:33:53 +0000

    update apaper_run

--- src/model/experiment_run.py
+++ src/model/experiment_run.py
@@ -651,5 +651,11 @@ def r_mat_corr(yerroblist, ytimestep, y_strlst, r_diag, corr=0.3, tau=1., cut_of
                         r_corr[i, j] = corr*np.exp(-(abs(float(ytimestep[i])-float(ytimestep[j]))**2)/float(tau)**2)
                     elif y_strlst[i] == 'nee_day' and y_strlst[j] == 'nee_night':
                         r_corr[i, j] = corr*np.exp(-(abs(float(ytimestep[i])-float(ytimestep[j]))**2)/float(tau)**2)
+        if y_strlst[i] == 'nee':
+            for j in xrange(len(ytimestep)):
+                if y_strlst[j] == 'nee':
+                    if abs(ytimestep[i]-ytimestep[j]) < cut_off:
+                        r_corr[i, j] = corr*np.exp(-(abs(float(ytimestep[i])-float(ytimestep[j]))**2)/float(tau)**2) \
+                                      + (1-corr)*smp.KroneckerDelta(ytimestep[i], ytimestep[j])
     r = np.dot(np.dot((np.sqrt(r_diag)), r_corr), np.sqrt(r_diag))
     return r_corr, r
