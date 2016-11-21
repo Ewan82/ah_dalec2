ad80e010f4dff99a796d4b5d9130f7c5dda72571
Ewan Pinnington <ewan.pinnington@gmail.com>
2016-11-21 09:46:46 +0000

    added new exp

--- src/model/paper_run.py
+++ src/model/paper_run.py
@@ -105,15 +105,17 @@ def experiment_bmat_ceff_fauto_ffol_flab2(f_name):
     # Construct B
     b_cor = pickle.load(open('b_edc_cor.p', 'r'))
     b_std = np.sqrt(np.diag(pickle.load(open('b_edc.p', 'r'))))
-    b_std[10] = 0.2*b_std[10]
-    b_std[1] = 0.2*b_std[1]
-    #b_std[2] = 0.2*b_std[2] # Maybe get rid of this constraint
-    #b_std[9] = 0.2*b_std[9]
-    #b_std[12] = 0.2*b_std[12]
-    #b_std[0:17] = b_std[0:17]
+    b_std[10] = 0.25*b_std[10]
+    b_std[1] = 0.25*b_std[1]
+    b_std[11] = 0.25*b_std[11] # Maybe get rid of this constraint
+    b_std[13] = 0.25*b_std[13]
+    b_std[14] = 0.25*b_std[14]
+    b_std[15] = 0.25*b_std[15]
+    b_std[4] = 0.25*b_std[4]
+    b_std[0:17] = b_std[0:17]*0.5
     D = np.zeros_like(b_cor)
     np.fill_diagonal(D, b_std)
-    b = np.dot(np.dot(D, b_cor), D)*0.5
+    b = np.dot(np.dot(D, b_cor), D)*0.8
     experiment(f_name, b)
     return 'done!'
 
