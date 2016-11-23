5f9642529166c6437c5def332821944b6a4abeca
Ewan Pinnington <ewan.pinnington@gmail.com>
2016-11-23 08:36:25 +0000

    added new exp

--- src/model/paper_run.py
+++ src/model/paper_run.py
@@ -106,20 +106,20 @@ def experiment_bmat_ceff_fauto_ffol_flab2(f_name):
     b_cor = pickle.load(open('b_edc_cor.p', 'r'))
     b_std = np.sqrt(np.diag(pickle.load(open('b_edc.p', 'r'))))
     #b_std[0] = 0.25*b_std[0]
-    b_std[10] = 0.25*b_std[10]
+    b_std[10] = 0.2*b_std[10]
     b_std[1] = 0.25*b_std[1]
     #b_std[4] = 0.3*b_std[4]  # start of LAI constraint
     #b_std[11] = 0.3*b_std[11]
-    #b_std[13] = 0.3*b_std[13]
+    b_std[13] = 0.3*b_std[13]
     #b_std[14] = 0.3*b_std[14]
     b_std[15] = 0.3*b_std[15]  # end of LAI constraint
     b_std[2] = 0.3*b_std[2]  # ffol, Maybe get rid of this constraint
-    b_std[12] = 0.25*b_std[12]  # flab, Maybe get rid of this constraint
+    b_std[12] = 0.3*b_std[12]  # flab, Maybe get rid of this constraint
     #b_std[7] = 0.25*b_std[7]
-    b_std[0:17] = b_std[0:17]
+    b_std[0:17] = b_std[0:17]*0.5
     D = np.zeros_like(b_cor)
     np.fill_diagonal(D, b_std)
-    b = np.dot(np.dot(D, b_cor), D)*0.8
+    b = np.dot(np.dot(D, b_cor), D)*1.1
     experiment(f_name, b)
     return 'done!'
 
