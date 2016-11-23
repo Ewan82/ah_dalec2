5f70d1c24683077b4ed88b5e17b4818cf0981728
Ewan Pinnington <ewan.pinnington@gmail.com>
2016-11-23 09:00:48 +0000

    added new exp

--- src/model/paper_run.py
+++ src/model/paper_run.py
@@ -111,7 +111,7 @@ def experiment_bmat_ceff_fauto_ffol_flab2(f_name):
     #b_std[4] = 0.3*b_std[4]  # start of LAI constraint
     #b_std[11] = 0.3*b_std[11]
     b_std[13] = 0.3*b_std[13]
-    #b_std[14] = 0.3*b_std[14]
+    b_std[14] = 0.3*b_std[14]
     b_std[15] = 0.3*b_std[15]  # end of LAI constraint
     b_std[2] = 0.3*b_std[2]  # ffol, Maybe get rid of this constraint
     b_std[12] = 0.3*b_std[12]  # flab, Maybe get rid of this constraint
@@ -119,7 +119,7 @@ def experiment_bmat_ceff_fauto_ffol_flab2(f_name):
     b_std[0:17] = b_std[0:17]*0.5
     D = np.zeros_like(b_cor)
     np.fill_diagonal(D, b_std)
-    b = np.dot(np.dot(D, b_cor), D)*1.1
+    b = np.dot(np.dot(D, b_cor), D)*0.8
     experiment(f_name, b)
     return 'done!'
 
